c4f3f995400c6041296be40236d14329bc144d66
ytnobody <ytnobody@gmail.com>
2014-07-20 16:04:36 +0900

    suppress unwanted output

--- lib/vznope/net.sh
+++ lib/vznope/net.sh
@@ -2,7 +2,7 @@ function vznet-init () {
     ip addr show $VZN_NETIF | grep "$VZN_CT_GATEWAY/$VZN_CT_NETMASK" > /dev/null 2>&1 ||
     ip addr add $VZN_CT_GATEWAY/$VZN_CT_NETMASK dev $VZN_NETIF
     vznet-create-chain vzn
-    iptables -S vzn-nat -t nat | grep $VZN_CT_NETWORK ||
+    iptables -S vzn-nat -t nat | grep $VZN_CT_NETWORK > /dev/null 2>&1 ||
         vznet-chain-rule vzn-nat -t nat -s $VZN_CT_NETWORK -j MASQUERADE
 }
 
