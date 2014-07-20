4327773fcd4300cf823330725cb079bf53440562
ytnobody <ytnobody@gmail.com>
2014-07-20 15:25:38 +0900

    add message for setupsh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ BINDIR=$PERLROOT/bin
 
 if [ -z "$VZN_ROOT" ]; then
     echo . $APPROOT/vznope.bashrc >> $HOME/.bash_profile
+    echo 'Please add a following line by visudo.'
+    echo $USER'        ALL=(root)      NOPASSWD: '$APPROOT'/bin/vzn'
     exec /bin/bash
 fi
 
