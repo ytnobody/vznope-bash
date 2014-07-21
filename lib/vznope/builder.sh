dbd1d051812b53dc4d228bbe5f496517f234f563
ytnobody <ytnobody@gmail.com>
2014-07-21 16:03:51 +0900

    add begin and end section

--- lib/vznope/builder.sh
+++ lib/vznope/builder.sh
@@ -2,20 +2,44 @@ function vznope-build () {
     ctid=$1 ; shift
     i=0
     cat - | 
-        sed '/^#/d; /^$/d;' | 
-        awk '{
+        sed '/^#/d; /^$/d; s/^ *//' | 
+        awk '
+        BEGIN {
+            is_exec_mode = 0;
+            exec_opts = "";
+        }
+        $0 ~ /^(begin|end)/ {
+            if ($0 ~ /^begin/) {
+                is_exec_mode = 1;
+                exec_opts = $0;
+                sub("begin", "", exec_opts);
+                sub(" ", "", exec_opts);
+            }
+            else {
+                is_exec_mode = 0;
+                exec_opts = "";
+            }
+            next;
+        }  
+        {
             getcommit = "'$VZN' commit-history '$ctid' "NR;
             getcommit | getline commit
             split(commit, part, "|");
             hash = part[1];
             author = part[2];
             message = part[3];
-            if ($0 == message || (message == "create" && $0 ~ /^create/)) {
+
+            run_cmd = $0;
+            if (is_exec_mode) {
+                run_cmd = exec_opts ? "exec " exec_opts " -- " $0 : "exec -- " $0;
+            }
+
+            if (run_cmd == message || (message == "create" && run_cmd ~ /^create/)) {
                 printf "\033[33mSKIP: %s\033[0m\n  already committed (%s by %s)\n", $0, hash, author;
             }
             else {
-                printf "\033[36mRUN: %s\033[0m\n", $0;
-                exitval = system("'$VZN' run '$ctid' "$0);
+                printf "\033[36mRUN: %s\033[0m\n", run_cmd;
+                exitval = system("'$VZN' run '$ctid' "run_cmd);
                 if (exitval) {
                     exit exitval;
                 }
@@ -24,7 +48,8 @@ function vznope-build () {
         }
         END {
             printf "\033[94mBuild finish.\033[0m\n";
-        }'
+        }
+    '
 }
 
 function vznope-run () {
